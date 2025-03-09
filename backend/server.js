7ac5068326dd1f191ffe05b32125c06397953c25
Pradeep Sajnani <82448770+Pradeep7867@users.noreply.github.com>
2025-03-09 16:46:36 +0530

    Server Config file updated

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,20 @@ const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 
+// Health Check Route
 app.get("/", (req, res) => {
   res.send("Excel-JSON Parser API is running...");
 });
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Export Express as a Vercel Serverless Function
+module.exports = app;
+
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
