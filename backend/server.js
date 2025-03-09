dd204819d00408e51e7c79008d24a2f037f81a7a
Pradeep Sajnani <82448770+Pradeep7867@users.noreply.github.com>
2025-03-09 16:52:03 +0530

    Config updated

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,21 @@
+require("dotenv").config(); // Load environment variables
+
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 
-// Health Check Route
 app.get("/", (req, res) => {
   res.send("Excel-JSON Parser API is running...");
 });
 
-// Export Express as a Vercel Serverless Function
-module.exports = app;
+// Use PORT from .env, fallback to 5000 for local
+const PORT = process.env.PORT || 5000;
 
-// const PORT = 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
